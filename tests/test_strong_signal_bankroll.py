58c50ff0bd74bcec793611e7b016c874f381dee0
Filip1994 <110042732+Filip1994@users.noreply.github.com>
2026-09-09 02:57:44 -0400

    style: satisfy ruff import ordering

--- tests/test_strong_signal_bankroll.py
+++ tests/test_strong_signal_bankroll.py
@@ -1,7 +1,4 @@
-from quantbot.strong_signal_bankroll import (
-    STRONG_SIGNAL_INITIAL_BANK,
-    portfolio,
-)
+from quantbot.strong_signal_bankroll import portfolio, STRONG_SIGNAL_INITIAL_BANK
 
 
 VIRTUAL = "STRONG_SIGNALS_VIRTUAL"
